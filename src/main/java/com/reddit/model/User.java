9aeb902ecde407420e0aa3c4d6e2ed00f18c85d3
Guru Vamshi <guruvamshi14@gmail.com>
2025-10-21 21:38:29 +0530

    Add user entity

---
+++ src/main/java/com/reddit/model/User.java
@@ -0,0 +1,40 @@
+package com.reddit.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String displayName;
+
+    @Column(nullable = false, unique = true)
+    private String uniqueName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private int karma = 0;
+
+    private String profileImageUrl;
+
+    private Instant createdDate = Instant.now();
+
+    @Column(length = 300)
+    private String bio;
+
+    private Instant lastLogin;
+}
